880a52ab96070281385490924cc1cb4a6c7e3441
My Master <derek.straga94@gmail.com>
2020-08-17 23:07:30 -0400

    Updates

---
+++ precache-manifest.efea5e952e80071cf5ac6d8a38be545b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f286f6e9bb6c35cc46d7c78d243dded",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0488dac433577252c5d",
+    "url": "/static/css/main.08c2e238.chunk.css"
+  },
+  {
+    "revision": "e6e5a300fef25eabb1c6",
+    "url": "/static/js/2.bd578bdb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bd578bdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0488dac433577252c5d",
+    "url": "/static/js/main.38c76d49.chunk.js"
+  },
+  {
+    "revision": "84ecbe957071f84116ba",
+    "url": "/static/js/runtime-main.cb6d9c40.js"
+  }
+]);
